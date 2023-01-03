7a2388653a96d8650e480fcfd06db033aee0d30f
Sunwoo Lee <slee20@stanford.edu>
2023-01-03 12:03:29 -0800

    added mobile and desktop svg

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c63766696f90ac65"; }
+/******/ 		__webpack_require__.h = function() { return "c8698a308b387ccd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
