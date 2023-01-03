353d27825f2ecefd9f3f7d25214d145f142072d6
Sunwoo Lee <slee20@stanford.edu>
2023-01-03 11:39:53 -0800

    add gitignore

---
+++ .next/server/font-loader-manifest.js
@@ -0,0 +1,12 @@
+self.__FONT_LOADER_MANIFEST={
+  "pages": {
+    "/_app": [
+      "static/media/ef42359082410918.p.woff2",
+      "static/media/a7ce3066b936c5b8.p.woff2"
+    ],
+    "/": [
+      "static/media/ef42359082410918.p.woff2"
+    ]
+  },
+  "app": {}
+}
