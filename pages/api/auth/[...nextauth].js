2a2cded7f1e4f9c0f44e9478bb13c965da310565
Sunwoo Lee <slee20@stanford.edu>
2023-01-20 17:43:12 -0800

    there are bugs with supabase

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,28 @@
+// pages/api/auth/[...nextauth].js
+import NextAuth from 'next-auth';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { PrismaClient } from '@prisma/client';
+import EmailProvider from 'next-auth/providers/email';
+import nodemailer from 'nodemailer';
+import { SupabaseAdapter } from "@next-auth/supabase-adapter"
+
+const prisma = new PrismaClient();
+
+// pages/api/auth/[...nextauth].js
+export default NextAuth({
+  providers: [
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: process.env.EMAIL_FROM,
+      maxAge: 10 * 60, // Magic links are valid for 10 min only
+    }),
+  ],
+   adapter: PrismaAdapter(prisma),
+});
