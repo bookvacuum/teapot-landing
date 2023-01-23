40a0a576bf26fc5e12e0225a131b17093f768a68
michellewong793 <michellewong793@gmail.com>
2023-01-23 12:05:12 -0800

    add poci page

---
+++ components/saveButton.js
@@ -0,0 +1,17 @@
+import styles from "../styles/poci.module.css";
+import {useState} from "react";
+
+const SaveButton = (props) => {
+const [isSaved, setSaved] = useState(false)
+  return (
+     <div> 
+        {isSaved ? <p>POCI Signed! You're all set. LFG! </p> : <p>Click to Sign POCI</p>}
+       <button className={styles.button} onClick={() => setSaved(!isSaved)}> {isSaved ? "POCI Signed" : "Sign POCI"} </button>
+     </div>
+
+  )
+};
+
+export default SaveButton;
+
+
